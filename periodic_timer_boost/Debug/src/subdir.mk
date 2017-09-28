0910a9e08f854d8b3aa96eaeb24962e5cffb592d
danielgav <danielgav@192.168.0.19>
2017-09-29 00:28:34 +0200

    revision compilando

--- periodic_timer_boost/Debug/src/subdir.mk
+++ periodic_timer_boost/Debug/src/subdir.mk
@@ -4,20 +4,20 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/timer_boost.cpp 
+../src/periodic_timer_boost.cpp 
 
 OBJS += \
-./src/timer_boost.o 
+./src/periodic_timer_boost.o 
 
 CPP_DEPS += \
-./src/timer_boost.d 
+./src/periodic_timer_boost.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -std=c++0x -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
