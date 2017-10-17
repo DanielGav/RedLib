24c3c0de8b8296e240c7b8ce2ff91d922eebcc29
danielgav <danielgav@192.168.0.19>
2017-10-18 00:22:52 +0200

    añadido boost test

---
+++ hola_mundo/src/hola_mundo.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : hola_mundo.cpp
+// Author      : Daniel
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
